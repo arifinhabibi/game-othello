438378bb73c3e5617b110acf08c21ca024712ca9
muhammad arifin habibi <arifinhabibi76@gmail.com>
2022-10-14 17:02:40 +0700

    save project

--- source/js/game.js
+++ source/js/game.js
@@ -1,6 +1,11 @@
 class Game {
     constructor(){
         this.gridGame = document.getElementById('grid-game')
+
+        this.playerChoice = []
+
+        this.botChoice = []
+
     }
     
     render(){
@@ -16,35 +21,51 @@ class Game {
         this.tiles.forEach((tile, index) => {
 
             if ((index + 1) == 28 || (index + 1) == 37) {
-                this.player(tile)
+                this.player(tile, index + 1)
             }
 
             if ((index + 1) == 29  || (index + 1) == 36) {
-                this.bot(tile)
+                this.bot(tile, index + 1)
             }
 
-            console.log(index+1 * index+1)
-
-            
-            
             tile.addEventListener('click', () => {
-                if (!tile.classList.contains('player') || !tile.classList.contains('bot')) {
+                if (!tile.classList.contains('player') && !tile.classList.contains('bot')) {
                     
-                    this.player(tile)
+                    this.player(tile, index+1)
                 
                 }
             })
         })
+
+
+
     }
 
-    player(tile){
+    // player action
+    player(tile, index){
         tile.classList.add('player')
+        this.playerChoice.push(index)
+        console.log(this.playerChoice)
+
+
+        this.playerScore = document.getElementById('scoring-player')
+
+        this.playerScore.innerHTML = this.playerChoice.length
+
     }
 
-    bot(tile){
+    // bot action
+    bot(tile, index){
         tile.classList.add('bot')
+        this.botChoice.push(index)
+
+        this.botScore = document.getElementById('scoring-bot')
+
+        this.botScore.innerHTML = this.botChoice.length
+
     }
-    
+
+    // membuat lantai 8 * 8
     tiles(){
         for (let row = 1; row <= 8; row++) {
             for (let col = 1; col <= 8; col++) {
@@ -61,3 +82,10 @@ class Game {
 const game = new Game()
 
 game.render()
+
+
+const reset = document.querySelector('.reset')
+
+reset.addEventListener('click', () => {
+    location.reload()
+})
