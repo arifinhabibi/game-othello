5d7512360dbe4b80a6f43fe6691dfa46d3a100ad
muhammad arifin habibi <arifinhabibi76@gmail.com>
2022-10-15 18:59:59 +0700

    save project

--- source/js/game.js
+++ source/js/game.js
@@ -3,65 +3,176 @@ class Game {
         this.gridGame = document.getElementById('grid-game')
 
         this.playerChoice = []
+        this.botChoice    = []
 
-        this.botChoice = []
+        this.cells = [];
+
+        this.dom = document.querySelector('.game-main');
+        this.displayName = document.querySelector('.display-name');
+
+        this.botScore = document.getElementById('scoring-bot')
+
+        this.playerScore = document.getElementById('scoring-player')
+
 
     }
     
     render(){
-        this.create()
-        
+        this.create() 
+
+    }
+
+    mount() {
+        this.dom.classList.add('active');
+
+        this.displayName.innerHTML = intro.inputName.value; 
+    }
+
+    unMount() {
+        this.dom.classList.remove('active');
     }
 
     create(){
         this.tiles()
+        
+        this.bot(4, 4)
+        // this.bot(3, 4)
+        // this.bot(2, 4)
+        this.bot(5, 5)
+        this.player(5, 4)
+        this.player(4, 5)
 
-        this.tiles = document.querySelectorAll('.tile')
+        
 
-        this.tiles.forEach((tile, index) => {
-
-            if ((index + 1) == 28 || (index + 1) == 37) {
-                this.player(tile, index + 1)
-            }
-
-            if ((index + 1) == 29  || (index + 1) == 36) {
-                this.bot(tile, index + 1)
-            }
-
-            tile.addEventListener('click', () => {
-                if (!tile.classList.contains('player') && !tile.classList.contains('bot')) {
-                    
-                    this.player(tile, index+1)
-                
-                }
-            })
-        })
-
-
+        this.generateSuggest();
 
     }
 
+    generateSuggest() {
+        // const botCells = this.cells.filter(cell => cell.dom.classList.contains('bot'));
+        const playerCells = this.cells.filter(cell => cell.dom.classList.contains('player'));
+
+
+
+        playerCells.forEach((players) => {
+
+
+            // cari data bot yang ad di atas
+            let top = players.row - 1
+            let stop = false
+            let disc = []
+             while(!stop){
+               let dataBotTop = this.cells.find((cel) => cel.dom.classList.contains('bot') && cel.col == players.col &&  cel.row == top)
+               if (dataBotTop) {
+                disc.push(dataBotTop)
+                top--
+               }else {
+                if (top == 0) {
+                    disc = []
+                }
+                stop = true
+               }
+
+           }
+            this.cells.find(cell2 => players.row !== top+1 && cell2.row === top && cell2.col === players.col)?.setSuggest(disc[disc.length - 1], 'top')
+
+            // cari data bot yang ada di kanan
+            let right = players.col + 1
+           stop = false
+           disc = []
+           while(!stop){
+                let dataBotRight = this.cells.find((cell) => cell.dom.classList.contains('bot') && cell.row == players.row && cell.col == right)
+                if (dataBotRight) {
+                    disc.push(dataBotRight)
+                    right++
+                } else {
+                    if (right == 0) {
+                        disc = []
+                    }
+                    stop= true
+                }
+            }
+           this.cells.find(cell2 => players.col !== right-1 && cell2.col === right && cell2.row == players.row)?.setSuggest(disc[disc.length - 1], 'right')
+
+
+            // cari data bot yang ad di kiri
+           let left = players.col - 1
+           stop = false
+           disc = []
+           while(!stop){
+                let dataBotLeft = this.cells.find((cell) => cell.dom.classList.contains('bot') && cell.row == players.row && cell.col == left)
+                if (dataBotLeft) {
+                    disc.push(dataBotLeft)
+                    left--
+                } else {
+                    if (left == 0) {
+                        disc = []
+                    }
+                    stop= true
+                }
+            }
+           this.cells.find(cell2 => players.col !== left+1 && cell2.col === left && cell2.row == players.row)?.setSuggest(disc[disc.length - 1], 'left')
+
+
+        //  cari data bot yang ada di bawah
+        let bottom = players.row + 1
+        stop = false
+        disc = []
+         while(!stop){
+           let dataBotBottom = this.cells.find((cel) => cel.dom.classList.contains('bot') && cel.col == players.col &&  cel.row == bottom)
+           if (dataBotBottom) {
+            disc.push(dataBotBottom)
+            bottom++
+           }else {
+            if (bottom == 0) {
+                disc = []
+            }
+            stop = true
+           }
+
+       }
+        this.cells.find(cell2 => players.row !== bottom-1 && cell2.row === bottom && cell2.col === players.col)?.setSuggest(disc[disc.length - 1], 'bottom')
+
+
+
+        })
+    }
+
+
+   
+
     // player action
-    player(tile, index){
-        tile.classList.add('player')
-        this.playerChoice.push(index)
-        console.log(this.playerChoice)
+    player(row, col){
+        
+        this.cells.forEach((cell) => {
+            if (cell.row == row && cell.col == col)  {
+                cell.dom.classList.add('player')
+                this.playerChoice.push(cell)
+            }
+        })
 
-
-        this.playerScore = document.getElementById('scoring-player')
 
         this.playerScore.innerHTML = this.playerChoice.length
+
+        
 
     }
 
     // bot action
-    bot(tile, index){
-        tile.classList.add('bot')
-        this.botChoice.push(index)
+    bot(row, col){
 
-        this.botScore = document.getElementById('scoring-bot')
+        this.cells.forEach((cell) => {
+            if (cell.row == row && cell.col == col) {
+                
+                cell.dom.classList.add('bot')
+                this.botChoice.push(cell)
+            }
+        })
 
-        this.botScore.innerHTML = this.botChoice.length
+
+        this.botScore.innerHTML = game.botChoice.length
+
+       
 
     }
 
@@ -69,19 +180,270 @@ class Game {
     tiles(){
         for (let row = 1; row <= 8; row++) {
             for (let col = 1; col <= 8; col++) {
-                const tile = document.createElement('div')
-                tile.classList.add('tile')
-                this.gridGame.append(tile)
+                const cell = new Cell(row, col);
+                cell.render();
+                this.cells.push(cell);
             }
         }
-        
     }
 
 }
 
-const game = new Game()
+class Cell {
+    constructor(row, col) {
+        this.row = row;
+        this.col = col;
+        this.dom = null;
+    }
+    render() {
+        this.create();
+        this.listen();
+    }
+    create() {
+        this.dom = document.createElement('div')
+        this.dom.setAttribute('data-col', this.col);
+        this.dom.setAttribute('data-row', this.row);
+        this.dom.classList.add('tile')
 
-game.render()
+        game.gridGame.append(this.dom)
+    }
+    listen() {
+        this.dom.addEventListener('click', () => {
+            if (!this.dom.classList.contains('player') && !this.dom.classList.contains('bot') && this.dom.classList.contains('step')) {
+                
+                game.player(this.row, this.col)
+                this.playerPoint(this.row, this.col, this.dom)
+            }
+        })
+    }
+
+    playerPoint(row, col, dom){
+
+        dom.classList.remove('step')
+
+        // cek bot yang ada bottom player
+        let bottom = row + 1
+        let discs = []
+        let stop = false
+
+        while(!stop){
+            game.cells.forEach((cell) => {
+                let data = game.botChoice.find((bot) => cell.dom.classList.contains('bot') && bot.col == col && bot.row == bottom)
+                // console.log(data)
+                if (data) {
+                    discs.push(data)
+                    bottom++
+                } else {
+                    stop = true
+                }
+            })
+        }
+
+        discs.forEach((disc) =>{
+            const point = game.cells.find(cell => cell.row == disc.row && cell.col == disc.col)
+            // console.log(disc)
+
+            if (point) {
+                point.dom.classList.remove('bot')
+
+                game.player(point.row, point.col)
+                
+                game.botChoice.splice(game.botChoice.indexOf(disc), 1)
+
+                game.botScore.innerHTML = game.botChoice.length
+            }
+        } )
+
+
+        // cek bot yang ada top player
+        let top = row - 1
+        discs = []
+        stop = false
+
+        while(!stop){
+            game.cells.forEach((cell) => {
+                let data = game.botChoice.find((bot) => cell.dom.classList.contains('bot') && bot.col == col && bot.row == top)
+                // console.log(data)
+                if (data) {
+                    discs.push(data)
+                    top--
+                } else {
+                    stop = true
+                }
+            })
+        }
+
+        discs.forEach((disc) =>{
+            const point = game.cells.find(cell => cell.row == disc.row && cell.col == disc.col)
+            // console.log(disc)
+
+            if (point) {
+                point.dom.classList.remove('bot')
+
+                game.player(point.row, point.col)
+                
+                game.botChoice.splice(game.botChoice.indexOf(disc), 1)
+
+                game.botScore.innerHTML = game.botChoice.length
+            }
+        } )
+
+       
+        // cek bot yang ada di left player
+        let  left = col + 1
+        discs = []
+        stop = false
+
+        while(!stop){
+            game.cells.forEach((cell) => {
+                let data = game.botChoice.find((bot) => cell.dom.classList.contains('bot') && bot.col == left && bot.row == row)
+                // console.log(data)
+                if (data) {
+                    discs.push(data)
+                    left++
+                } else {
+                    stop = true
+                }
+            })
+        }
+        // console.log(game.botChoice)
+
+        discs.forEach((disc) =>{
+            const point = game.cells.find(cell => cell.row == disc.row && cell.col == disc.col)
+            // console.log(disc)
+
+            if (point) {
+                point.dom.classList.remove('bot')
+
+                game.player(point.row, point.col)
+                
+                game.botChoice.splice(game.botChoice.indexOf(disc), 1)
+
+                game.botScore.innerHTML = game.botChoice.length
+            }
+        } )
+
+        // cek bot yang ada di right player
+        let  right = col - 1
+        discs = []
+        stop = false
+
+        while(!stop){
+            game.cells.forEach((cell) => {
+                let data = game.botChoice.find((bot) => cell.dom.classList.contains('bot') && bot.col == right && bot.row == row)
+                // console.log(data)
+                if (data) {
+                    discs.push(data)
+                    right++
+                } else {
+                    stop = true
+                }
+            })
+        }
+        // console.log(game.botChoice)
+
+        discs.forEach((disc) =>{
+            const point = game.cells.find(cell => cell.row == disc.row && cell.col == disc.col)
+            // console.log(disc)
+
+            if (point) {
+                point.dom.classList.remove('bot')
+
+                game.player(point.row, point.col)
+                
+                game.botChoice.splice(game.botChoice.indexOf(disc), 1)
+
+                game.botScore.innerHTML = game.botChoice.length
+            }
+        } )
+
+    }
+    
+    setSuggest(cellFrom, position){
+
+        if (position == 'top') {
+            const suggest = game.cells.find(cell => cell.col === cellFrom.col && cell.row === cellFrom.row - 1);
+            suggest.dom.classList.add('step')     
+        }
+        
+        if (position == 'left') {
+            const suggest = game.cells.find(cell => cell.row == cellFrom.row && cell.col == cellFrom.col - 1) 
+            suggest.dom.classList.add('step')     
+        }
+
+        if (position == 'right') {
+            const suggest = game.cells.find(cell => cell.row == cellFrom.row && cell.col == cellFrom.col + 1) 
+            suggest.dom.classList.add('step')
+        }
+
+        if (position == 'bottom') {
+            const suggest = game.cells.find(cell => cell.col === cellFrom.col && cell.row === cellFrom.row + 1);
+            suggest.dom.classList.add('step')
+        }
+
+
+        
+    }
+
+    
+   
+}
+
+class Intro {
+    constructor() {
+        this.dom = document.querySelector('.intro')
+        this.btnStart = document.querySelector('.start-game')
+        this.inputName = document.querySelector('.input-name')
+    }
+    render(){ 
+        this.listen();
+    }
+    mount() {
+        this.dom.classList.add('active');
+    }
+
+    unMount() {
+        this.dom.classList.remove('active');
+    }
+    listen() {
+        this.btnStart.addEventListener('click', () => {
+            othello.setActiveScreen('game');
+        })
+    }
+}
+
+ 
+const game = new Game()
+game.render() 
+
+const intro = new Intro()
+intro.render()
+
+
+class Othello{
+    constructor() {
+        this.screens = {
+            game,  
+            intro,
+        }
+        this.activeScreen = null;
+    }
+    render() {
+        this.setActiveScreen('game');
+
+        
+
+        
+    }
+    setActiveScreen(screen) {
+        if (this.activeScreen) { this.screens[this.activeScreen].unMount() }
+        this.screens[screen]?.mount();
+        this.activeScreen = screen;
+    }
+}
+
+const othello = new Othello();
+othello.render();
 
 
 const reset = document.querySelector('.reset')
